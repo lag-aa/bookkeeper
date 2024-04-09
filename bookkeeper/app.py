96d048aabcdf125e3c0580f98fa558764a6e0fa0
Aleksandr Lagutenko <lag.aa.418@gmail.com>
2024-04-09 05:40:52 +0300

    feat: Create a database add check on startup

--- bookkeeper/app.py
+++ bookkeeper/app.py
@@ -2,12 +2,24 @@
 
 # pylint: disable=E0611
 import sys
+import os
+import configparser
 from PySide6.QtWidgets import QApplication
 from bookkeeper.views.home import MainWindow
+from bookkeeper.scripts.create_db import create_database
+
+config = configparser.ConfigParser()
+config.read("bookkeeper/config/settings.ini")
+db_name = config["sqllite"]["db_name"]
 
 
 def main():
     """Main function to launch the application."""
+
+    if not os.path.exists(db_name):
+        # Если файл не существует, создаем базу данных
+        create_database(db_name)
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
